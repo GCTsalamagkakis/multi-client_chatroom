181809dd53b04910f8b6fcafd9ae1560a981d299
George-Chris Tsalamagkakis <tsalamagkakisgc@gmail.com>
2018-05-27 21:48:10 +0300

    Add files via upload

---
+++ src/client.py
@@ -0,0 +1,96 @@
+import socket
+import select
+import string
+import sys
+import os
+import sqlite3
+ 
+def prompt() :
+    sys.stdout.write('<You> ')
+    sys.stdout.flush()
+ 
+#main function
+if __name__ == "__main__":
+     
+    conn = sqlite3.connect("users.db")
+    dbcursor = conn.cursor()
+    command = '''CREATE TABLE IF NOT EXISTS users ('username' text UNIQUE, 'password' text)'''
+    dbcursor.execute(command)
+
+     
+    logreg=input("press 1 to log in/press 2 to regiser: ") 
+    if (logreg==2):
+        while True:
+            usernamez=raw_input("Select a Username: ")
+            passw=raw_input("Select a Password: ")
+            command = '''SELECT username FROM users
+                         WHERE username=?'''
+            dbcursor.execute(command, (usernamez,))
+            result = dbcursor.fetchone()
+            if result:
+                print "This username already exists"
+            else:
+                command = '''INSERT INTO users VALUES (?, ?)'''
+                dbcursor.execute(command, (usernamez, passw))
+                conn.commit()
+                print "You have succesfully registered"
+                break
+        
+    if logreg==1:
+        counter=0
+        found=-1
+        while True:
+            usernamez=raw_input("Give Me Your Username: ")
+            passw=raw_input("Give Me Your Password: ")
+            command = '''SELECT username, password FROM users
+                         WHERE username=? and password=?'''
+            dbcursor.execute(command, (usernamez, passw))
+            result = dbcursor.fetchone()
+            if result:
+                print "You have now logged in!"
+                break
+            else:
+                print "the username and/or password are incorrect"
+        
+
+    host = sys.argv[1]
+    port = int(sys.argv[2])
+     
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(2)
+     
+    # connect to remote host
+    try :
+        s.connect((host, port))
+    except :
+        print ('Unable to connect')
+        sys.exit()
+     
+    print ('Connected to remote host. Start sending messages (@username for private message)')
+    s.send(usernamez.encode())
+    prompt()
+    
+     
+    while 1:
+        socket_list = [sys.stdin, s]
+         
+        # Get the list sockets which are readable
+        read_sockets, write_sockets, error_sockets = select.select(socket_list , [], [])
+         
+        for sock in read_sockets:
+            #incoming message from remote server
+            if (sock == s):
+                data = sock.recv(4096)
+                if (not data) :
+                    print ('\nDisconnected from chat server')
+                    sys.exit()
+                else :
+                    #print data
+                    sys.stdout.write(data)
+                    prompt()
+             
+            #user entered a message
+            else :
+                msg = sys.stdin.readline()
+                s.send(msg)
+                prompt()
---
+++ src/server.py
@@ -0,0 +1,90 @@
+import socket
+import select
+ 
+def broadcast_data (sock, message):
+    #send message to everyone except server and myself
+    for socket in CONNECTION_LIST:
+        if socket != server_socket and socket != sock :
+            try :
+                socket.send(message)
+            except :
+                socket.close()
+                CONNECTION_LIST.remove(socket)
+                
+def private_data (sock, message):
+    #send message in specific user
+    for socket in CONNECTION_LIST:
+        if socket == sock :
+            try :
+                socket.send(message)
+            except :
+                socket.close()
+                CONNECTION_LIST.remove(socket)
+ 
+if __name__ == "__main__":
+     
+    
+    CONNECTION_LIST = []
+    RECV_BUFFER = 4096 
+    PORT = 5000
+    username = []
+    #create TCP connection
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind(("0.0.0.0", PORT))
+    server_socket.listen(10)
+ 
+    CONNECTION_LIST.append(server_socket)
+    username.append("server")
+ 
+    print("Chat server started on port " + str(PORT))
+ 
+    while 1:
+        
+        read_sockets,write_sockets,error_sockets = select.select(CONNECTION_LIST,[],[])
+ 
+        for sock in read_sockets:
+
+            if sock == server_socket:
+                
+                sockfd, addr = server_socket.accept()
+                CONNECTION_LIST.append(sockfd)
+                read_sockets,write_sockets,error_sockets = select.select(CONNECTION_LIST,[],[])
+                print ("Client (%s, %s) connected" % addr)
+                name=sockfd.recv(RECV_BUFFER).decode("ascii")
+                print (name)
+                username.append(name)
+            else:
+                try:
+                    data = sock.recv(RECV_BUFFER)
+                    if data:
+                        private = False
+                        cell=CONNECTION_LIST.index(sock)
+						#send private message
+                        if data[0] == "@":
+                            to_who=data.split(None,1)
+                            
+                            for target in username:
+                                if to_who[0][1:]==target:
+                                    message = data.split()
+                                    to_send = ""
+									#exclude username from message
+                                    for word in message[1:]:
+                                        to_send+= word + " "
+                                    to_send+= "\n"
+                                    send_to=username.index(target)
+                                    private_data(CONNECTION_LIST[send_to],"\r" + "<%s> (private) " % username[cell] + to_send)
+                                    private = True
+						#no reason to broadcast
+                        if private:
+                            continue
+                        broadcast_data(sock, "\r" + "<%s> " % username[cell] + data)                
+                 
+                except:
+                    print ("Client (%s, %s) is offline" % addr)
+                    sock.close()
+                    CONNECTION_LIST.remove(sock)
+                    continue
+     
+    server_socket.close()
